d8a682946aa073adb6488c1a53ad12e517d3e0b2
Mogade <atharva.mogade@wolterskluwer.com>
2024-05-21 21:19:43 +0530

    Implement dynamic table and no content in table message

--- src/app/cpa-table/cpa-table.component.ts
+++ src/app/cpa-table/cpa-table.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JumpstartComponentsModule } from '@wk/components-angular15';
 
+interface cpaData {
+  dependantPractionerName: string;
+  credentials: string;
+}
 @Component({
   selector: 'app-cpa-table',
   standalone: true,
@@ -9,6 +13,22 @@ import { JumpstartComponentsModule } from '@wk/components-angular15';
   templateUrl: './cpa-table.component.html',
   styleUrl: './cpa-table.component.css'
 })
+
+
 export class CPATableComponent {
 
+  cpaDummyData: cpaData[] = [
+    {
+    "dependantPractionerName": "Phillip Lawerence",
+    "credentials": "PharD"
+    },
+    {
+      "dependantPractionerName": "Henri Severin",
+      "credentials": "Nurse Practioner"
+    }, 
+    {
+      "dependantPractionerName": "Olga Severin",
+      "credentials": "Physicians Assistant"
+    }
+  ];
 }
