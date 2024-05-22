c62145495884a3b4dff269290df1198b3ea7f7a8
Mogade <atharva.mogade@wolterskluwer.com>
2024-05-22 16:47:37 +0530

    Fix html tbody tag closing

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JumpstartComponentsModule } from '@wk/components-angular15';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [CommonModule, JumpstartComponentsModule],
+  imports: [CommonModule, JumpstartComponentsModule, RouterModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
