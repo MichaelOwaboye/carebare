a6edaead682da83488e8fc08ae5b60024d1b1615
Mogade <atharva.mogade@wolterskluwer.com>
2024-05-22 23:48:58 +0530

    Close Button on cpa-document

--- src/app/cpa-document/cpa-document.component.ts
+++ src/app/cpa-document/cpa-document.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { JumpstartComponentsModule } from '@wk/components-angular15';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cpa-document',
   standalone: true,
-  imports: [JumpstartComponentsModule],
+  imports: [JumpstartComponentsModule, RouterModule],
   templateUrl: './cpa-document.component.html',
   styleUrl: './cpa-document.component.css'
 })
